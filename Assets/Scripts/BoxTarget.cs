046fa9377c0f1eb5a7e13a76d959e3baa48554d5
akc8012 <akcolannino@email.neit.edu>
2018-01-27 18:11:53 -0500

    box target for scoring idk

--- Assets/Scripts/BoxTarget.cs
+++ Assets/Scripts/BoxTarget.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class BoxTarget : MonoBehaviour
 {
-
-	void Start()
-	{
-		
-	}
+	[SerializeField] ScoreDisplay scoreDisplay;
+	[SerializeField] float destroyTimer = 1;
 
 	void OnTriggerEnter(Collider col)
 	{
-		
+		if (col.tag == "Interactable")
+		{
+			Interactable box = col.GetComponent<Interactable>();
+			scoreDisplay.AddScore(col.GetComponent<Interactable>().GetPoints);
+			StartCoroutine(DestroyBoxAfterTime(box, destroyTimer));
+		}
+	}
+
+	IEnumerator DestroyBoxAfterTime(Interactable box, float time)
+	{
+		yield return new WaitForSeconds(time);
+		Destroy(box.gameObject);
 	}
 }
