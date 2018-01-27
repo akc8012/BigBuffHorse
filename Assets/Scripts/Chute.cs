ddbdc403a041971abda9babb97739c0ad8ef41b8
B1on1cBen <bengg2015@gmail.com>
2018-01-27 10:54:55 -0500

    Added chute

---
+++ Assets/Scripts/Chute.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Chute : MonoBehaviour
+{
+	[SerializeField] GameObject[] spawnables;
+	[SerializeField] float minSpawnDelay;
+	[SerializeField] float maxSpawnDelay;
+
+	float spawnTimer;
+
+	Transform spawnLoc;
+	Transform boxContainer;
+
+	void Awake()
+	{
+		spawnLoc = transform.FindChild("Spawn Loc");
+		boxContainer = GameObject.Find("Boxes").transform;
+		SetNextSpawnTime();
+	}
+
+	void Update()
+	{
+		if (Time.time >= spawnTimer)
+		{
+			SpawnObject();
+			SetNextSpawnTime();
+		}
+	}
+
+	public void SpawnObject()
+	{
+		int randNum = Random.Range(0, spawnables.Length);
+
+		GameObject clone = Instantiate(spawnables[randNum], spawnLoc.position, Quaternion.identity, boxContainer);
+	}
+
+	void SetNextSpawnTime()
+	{
+		spawnTimer = Time.time + Random.Range(minSpawnDelay, maxSpawnDelay);
+	}
+}
