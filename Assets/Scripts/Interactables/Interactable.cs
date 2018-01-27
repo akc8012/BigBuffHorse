b5d8142b49f82227b3f5075cbbac13c4bfdcf030
B1on1cBen <bengg2015@gmail.com>
2018-01-27 15:45:25 -0500

    Added preliminary pickup logic

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -7,9 +7,11 @@ public enum InteractableSize
 	Large
 }
 
-public class Interactable : MonoBehaviour
+public abstract class Interactable : MonoBehaviour
 {
 	[SerializeField] InteractableSize size;
 
-
+	// Function to be triggered when player picks up this object.
+	// I.E., bomb starts fuse.
+	public abstract void OnPickup();
 }
---
+++ Assets/Scripts/Pickup/PlayerPickup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerPickup : MonoBehaviour {
+
+	Interactable heldItem;
+	Interactable seenItem;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnPickupStay(Collider pickup) {
+		
+	}
+
+	public void OnPickupEnter(Collider pickup) {
+		Debug.Log("I see package!");
+	}
+}
