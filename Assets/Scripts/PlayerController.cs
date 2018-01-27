2c54d3d3adc1e923587cfd903b365be8426a455a
akc8012 <akcolannino@email.neit.edu>
2018-01-26 21:06:32 -0500

    test thing idk

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	void Start()
+	{
+		
+	}
+	
+	void Update()
+	{
+		if (Input.GetAxisRaw("Horizontal") >= 0.5f)
+			print("memes");
+	}
+}
