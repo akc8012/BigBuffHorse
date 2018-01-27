9aea66b21fa1f3988e75b3dfcbf9915f71f11379
akc8012 <akcolannino@email.neit.edu>
2018-01-26 21:53:50 -0500

    some garbage player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	CharacterController characterController;
+	float rotSmooth = 45;
+	float speed = 0.15f;
 
-	void Start()
+	void Awake()
 	{
-		
+		characterController = GetComponent<CharacterController>();
 	}
-	
+
 	void Update()
 	{
-		if (Input.GetAxisRaw("Horizontal") >= 0.5f)
-			print("memes");
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+		if (Vector3.Angle(input, transform.forward) > 135)
+			transform.forward = input;
+		else
+		{
+			Vector3 targetRotation = Vector3.Lerp(transform.forward, input, Time.deltaTime * rotSmooth);
+			if (targetRotation != Vector3.zero)
+				transform.rotation = Quaternion.LookRotation(targetRotation);
+		}
+
+
+
+		characterController.Move(transform.forward * input.magnitude * speed);
 	}
 }
