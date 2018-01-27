05346785759241f94636e90a6a7b78ae5abf9e15
akc8012 <akcolannino@email.neit.edu>
2018-01-27 14:36:20 -0500

    pushed all my stuff again lel

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,15 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+	[SerializeField]
+	GameObject gameStateManager;
+
+	void Awake()
+	{
+		if (GameStateManager.instance == null)
+			Instantiate(gameStateManager);
+	}
+}
