780ad8b6eba59363dd00ca78e74a012341b7fbbb
akc8012 <akcolannino@email.neit.edu>
2018-01-27 14:11:48 -0500

    singleton setup, dumb menu stuff

---
+++ Assets/Scripts/BasicMenuUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BasicMenuUI : MonoBehaviour
+{
+	[SerializeField] Button playAgainButt;
+	[SerializeField] Button quitButt;
+
+	void Start()
+	{
+		if (playAgainButt != null)
+			playAgainButt.onClick.AddListener(RestartGameClicked);
+		
+		if (quitButt != null)
+			quitButt.onClick.AddListener(GoToTitleClicked);
+	}
+
+	void RestartGameClicked()
+	{
+		GameStateManager.instance.StartGame();
+	}
+
+	void GoToTitleClicked()
+	{
+		GameStateManager.instance.GoToTitle();
+	}
+
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour
+{
+	[SerializeField] GameObject gameStateManager;
+	
+	void Awake()
+	{
+		if (GameStateManager.instance == null)
+			Instantiate(gameStateManager);
+	}
+}
