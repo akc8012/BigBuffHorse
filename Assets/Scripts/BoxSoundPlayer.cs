8143d0a3c579b21f1016c10b13b0195a7b9086c3
akc8012 <akcolannino@email.neit.edu>
2018-01-28 09:42:32 -0500

    box sounds and script idk

---
+++ Assets/Scripts/BoxSoundPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxSoundPlayer : MonoBehaviour
+{
+
+	void OnCollisionEnter(Collision col)
+	{
+		SoundManager.instance.PlayClip("boxSlam" + Random.Range(0, 4), 0.5f);
+	}
+}
